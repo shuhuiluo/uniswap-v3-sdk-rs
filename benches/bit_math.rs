732bfef5ce720b84aa7e069712842531a6b77943
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2023-12-29 23:27:27 -0800

    Add benchmark tests for bit math functions
    This update introduces benchmark tests for most and least significant bit functions. This functionality will assist in improving and optimizing these specific bit math operations. Additionally, the "criterion" crate is added to the Cargo.toml and Cargo.lock files, providing necessary benchmarking tools.

---
+++ benches/bit_math.rs
@@ -0,0 +1,54 @@
+use alloy_primitives::U256;
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::ops::Shl;
+use uniswap_v3_math::{bit_math, utils::ruint_to_u256};
+use uniswap_v3_sdk_rs::utils::{least_significant_bit, most_significant_bit};
+
+fn most_significant_bit_benchmark(c: &mut Criterion) {
+    c.bench_function("most_significant_bit", |b| {
+        b.iter(|| {
+            for i in 1u8..=255 {
+                let _ = most_significant_bit(U256::from(1).shl(i));
+            }
+        })
+    });
+}
+
+fn most_significant_bit_benchmark_ref(c: &mut Criterion) {
+    c.bench_function("most_significant_bit_ref", |b| {
+        b.iter(|| {
+            for i in 1u8..=255 {
+                let _ = bit_math::most_significant_bit(ruint_to_u256(U256::from(1).shl(i)));
+            }
+        })
+    });
+}
+
+fn least_significant_bit_benchmark(c: &mut Criterion) {
+    c.bench_function("least_significant_bit", |b| {
+        b.iter(|| {
+            for i in 1u8..=255 {
+                let _ = least_significant_bit(U256::from(1).shl(i));
+            }
+        });
+    });
+}
+
+fn least_significant_bit_benchmark_ref(c: &mut Criterion) {
+    c.bench_function("least_significant_bit_ref", |b| {
+        b.iter(|| {
+            for i in 1u8..=255 {
+                let _ = bit_math::least_significant_bit(ruint_to_u256(U256::from(1).shl(i)));
+            }
+        });
+    });
+}
+
+criterion_group!(
+    benches,
+    most_significant_bit_benchmark,
+    least_significant_bit_benchmark,
+    most_significant_bit_benchmark_ref,
+    least_significant_bit_benchmark_ref
+);
+criterion_main!(benches);
