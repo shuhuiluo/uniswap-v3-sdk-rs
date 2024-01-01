4fd45d838a9fadc956f4474447fd2dc1369f910d
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-01-01 05:01:45 -0800

    Add `TickListDataProvider` and update `tick_list`
    Introduced the `TickListDataProvider`, which gives tick data via an in-memory array. In `tick_list.rs`, replaced the use of `add_delta` function with direct arithmetic addition for efficiency. Also exported the `TickListDataProvider` in the `entities` module.

--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,7 +1,9 @@
 mod pool;
 mod tick;
 mod tick_data_provider;
+mod tick_list_data_provider;
 
 pub use pool::Pool;
 pub use tick::{Tick, TickTrait};
 pub use tick_data_provider::{NoTickDataError, NoTickDataProvider, TickDataProvider};
+pub use tick_list_data_provider::TickListDataProvider;
