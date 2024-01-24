5fbedaec29aa1ec9a0e37e2ae67a4e06002f5691
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-01-23 21:36:18 -0800

    Add multicall functionality
    A new module `multicall` has been added to provide multicall functionality within the `uniswap-v3-sdk`. This includes the creation of a new `multicall` solidity function and an `encode_multicall` function for encoding the input data. Associated tests for these functions have been included as well. Also, the package version has been updated from 0.14.0 to 0.15.0.

---
+++ src/multicall.rs
@@ -0,0 +1,38 @@
+use alloy_sol_types::{sol, SolCall};
+
+sol! {
+    function multicall(bytes[] data) external payable returns (bytes[] results);
+}
+
+pub fn encode_multicall(data: Vec<Vec<u8>>) -> Vec<u8> {
+    if data.len() == 1 {
+        data[0].clone()
+    } else {
+        let multicall = multicallCall { data };
+        multicall.abi_encode()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::hex;
+
+    #[test]
+    fn test_encode_multicall_string_array_len_1() {
+        let calldata = encode_multicall(vec![vec![0x01]]);
+        assert_eq!(calldata, vec![0x01]);
+    }
+
+    #[test]
+    fn test_encode_multicall_string_array_len_2() {
+        let calldata = encode_multicall(vec![
+            hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").to_vec(),
+            hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb").to_vec(),
+        ]);
+        assert_eq!(
+            calldata,
+            hex!("ac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000020aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0000000000000000000000000000000000000000000000000000000000000020bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+        );
+    }
+}
