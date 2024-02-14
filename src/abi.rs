82b9c454ff3b05a6678415133a4ba537795e129f
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-02-14 03:40:15 -0500

    Add route encoding to path and update dependencies
    Added a new utility function to encode routes to paths. This is useful when making swaps and trades on Uniswap's routes. Updated several dependencies to their latest versions, including the uniswap-sdk-core. Also created relevant tests for the new utility.

--- src/abi.rs
+++ src/abi.rs
@@ -150,4 +150,82 @@ sol! {
             uint256 amountRequested
         ) external returns (uint256 reward);
     }
+
+    interface IQuoter {
+        function quoteExactInput(bytes memory path, uint256 amountIn) external returns (uint256 amountOut);
+
+        function quoteExactInputSingle(
+            address tokenIn,
+            address tokenOut,
+            uint24 fee,
+            uint256 amountIn,
+            uint160 sqrtPriceLimitX96
+        ) external returns (uint256 amountOut);
+
+        function quoteExactOutput(bytes memory path, uint256 amountOut) external returns (uint256 amountIn);
+
+        function quoteExactOutputSingle(
+            address tokenIn,
+            address tokenOut,
+            uint24 fee,
+            uint256 amountOut,
+            uint160 sqrtPriceLimitX96
+        ) external returns (uint256 amountIn);
+    }
+}
+
+sol! {
+    interface IQuoterV2 {
+        function quoteExactInput(bytes memory path, uint256 amountIn)
+            external
+            returns (
+                uint256 amountOut,
+                uint160[] memory sqrtPriceX96AfterList,
+                uint32[] memory initializedTicksCrossedList,
+                uint256 gasEstimate
+            );
+
+        struct QuoteExactInputSingleParams {
+            address tokenIn;
+            address tokenOut;
+            uint256 amountIn;
+            uint24 fee;
+            uint160 sqrtPriceLimitX96;
+        }
+
+        function quoteExactInputSingle(QuoteExactInputSingleParams memory params)
+            external
+            returns (
+                uint256 amountOut,
+                uint160 sqrtPriceX96After,
+                uint32 initializedTicksCrossed,
+                uint256 gasEstimate
+            );
+
+        function quoteExactOutput(bytes memory path, uint256 amountOut)
+            external
+            returns (
+                uint256 amountIn,
+                uint160[] memory sqrtPriceX96AfterList,
+                uint32[] memory initializedTicksCrossedList,
+                uint256 gasEstimate
+            );
+
+        struct QuoteExactOutputSingleParams {
+            address tokenIn;
+            address tokenOut;
+            uint256 amount;
+            uint24 fee;
+            uint160 sqrtPriceLimitX96;
+        }
+
+        function quoteExactOutputSingle(QuoteExactOutputSingleParams memory params)
+            external
+            returns (
+                uint256 amountIn,
+                uint160 sqrtPriceX96After,
+                uint32 initializedTicksCrossed,
+                uint256 gasEstimate
+            );
+    }
 }
---
+++ src/utils/encode_route_to_path.rs
@@ -0,0 +1,257 @@
+use crate::prelude::Route;
+use alloy_sol_types::{sol, SolType, SolValue};
+use uniswap_sdk_core::prelude::*;
+
+type TokenFee = sol!((address, uint24));
+
+/// Converts a route to a hex encoded path
+///
+/// ## Arguments
+///
+/// * `route`: the v3 path to convert to an encoded path
+/// * `exact_output`: whether the route should be encoded in reverse, for making exact output swaps
+///
+pub fn encode_route_to_path<TInput: CurrencyTrait, TOutput: CurrencyTrait, P>(
+    route: Route<TInput, TOutput, P>,
+    exact_output: bool,
+) -> Vec<u8> {
+    let mut path: Vec<u8> = Vec::with_capacity(23 * route.pools.len() + 20);
+    if exact_output {
+        let mut output_token: Token = route.output.wrapped();
+        for pool in route.pools.iter().rev() {
+            let leg = if pool.token0.equals(&output_token) {
+                output_token = pool.token1.clone();
+                (pool.token0.address(), pool.fee as u32)
+            } else {
+                output_token = pool.token0.clone();
+                (pool.token1.address(), pool.fee as u32)
+            };
+            path.extend(TokenFee::abi_encode_packed(&leg));
+        }
+        path.extend(route.input.address().abi_encode_packed());
+    } else {
+        let mut input_token: Token = route.input.wrapped();
+        for pool in route.pools.iter() {
+            let leg = if pool.token0.equals(&input_token) {
+                input_token = pool.token1.clone();
+                (pool.token0.address(), pool.fee as u32)
+            } else {
+                input_token = pool.token0.clone();
+                (pool.token1.address(), pool.fee as u32)
+            };
+            path.extend(TokenFee::abi_encode_packed(&leg));
+        }
+        path.extend(route.output.address().abi_encode_packed());
+    }
+    path
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::prelude::*;
+    use alloy_primitives::hex;
+    use once_cell::sync::Lazy;
+    use uniswap_sdk_core::token;
+
+    static ETHER: Lazy<Ether> = Lazy::new(|| Ether::on_chain(1));
+    static TOKEN0: Lazy<Token> = Lazy::new(|| {
+        token!(
+            1,
+            "0x0000000000000000000000000000000000000001",
+            18,
+            "t0",
+            "token0"
+        )
+    });
+    static TOKEN1: Lazy<Token> = Lazy::new(|| {
+        token!(
+            1,
+            "0x0000000000000000000000000000000000000002",
+            18,
+            "t1",
+            "token1"
+        )
+    });
+    static TOKEN2: Lazy<Token> = Lazy::new(|| {
+        token!(
+            1,
+            "0x0000000000000000000000000000000000000003",
+            18,
+            "t2",
+            "token2"
+        )
+    });
+    static WETH: Lazy<Token> = Lazy::new(|| ETHER.wrapped());
+
+    static POOL_0_1_MEDIUM: Lazy<Pool<NoTickDataProvider>> = Lazy::new(|| {
+        Pool::new(
+            TOKEN0.clone(),
+            TOKEN1.clone(),
+            FeeAmount::MEDIUM,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_1_2_LOW: Lazy<Pool<NoTickDataProvider>> = Lazy::new(|| {
+        Pool::new(
+            TOKEN1.clone(),
+            TOKEN2.clone(),
+            FeeAmount::LOW,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_0_WETH: Lazy<Pool<NoTickDataProvider>> = Lazy::new(|| {
+        Pool::new(
+            TOKEN0.clone(),
+            WETH.clone(),
+            FeeAmount::MEDIUM,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+    static POOL_1_WETH: Lazy<Pool<NoTickDataProvider>> = Lazy::new(|| {
+        Pool::new(
+            TOKEN1.clone(),
+            WETH.clone(),
+            FeeAmount::MEDIUM,
+            encode_sqrt_ratio_x96(1, 1),
+            0,
+        )
+        .unwrap()
+    });
+
+    static ROUTE_0_1: Lazy<Route<Token, Token, NoTickDataProvider>> = Lazy::new(|| {
+        Route::new(
+            vec![POOL_0_1_MEDIUM.clone()],
+            TOKEN0.clone(),
+            TOKEN1.clone(),
+        )
+    });
+    static ROUTE_0_1_2: Lazy<Route<Token, Token, NoTickDataProvider>> = Lazy::new(|| {
+        Route::new(
+            vec![POOL_0_1_MEDIUM.clone(), POOL_1_2_LOW.clone()],
+            TOKEN0.clone(),
+            TOKEN2.clone(),
+        )
+    });
+
+    static ROUTE_0_WETH: Lazy<Route<Token, Ether, NoTickDataProvider>> =
+        Lazy::new(|| Route::new(vec![POOL_0_WETH.clone()], TOKEN0.clone(), ETHER.clone()));
+    static ROUTE_0_1_WETH: Lazy<Route<Token, Ether, NoTickDataProvider>> = Lazy::new(|| {
+        Route::new(
+            vec![POOL_0_1_MEDIUM.clone(), POOL_1_WETH.clone()],
+            TOKEN0.clone(),
+            ETHER.clone(),
+        )
+    });
+    static ROUTE_WETH_0: Lazy<Route<Ether, Token, NoTickDataProvider>> =
+        Lazy::new(|| Route::new(vec![POOL_0_WETH.clone()], ETHER.clone(), TOKEN0.clone()));
+    static ROUTE_WETH_0_1: Lazy<Route<Ether, Token, NoTickDataProvider>> = Lazy::new(|| {
+        Route::new(
+            vec![POOL_0_WETH.clone(), POOL_0_1_MEDIUM.clone()],
+            ETHER.clone(),
+            TOKEN1.clone(),
+        )
+    });
+
+    #[test]
+    fn pack_them_for_exact_input_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1.clone(), false),
+            hex!("0000000000000000000000000000000000000001000bb80000000000000000000000000000000000000002")
+        );
+    }
+
+    #[test]
+    fn pack_them_for_exact_output_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1.clone(), true),
+            hex!("0000000000000000000000000000000000000002000bb80000000000000000000000000000000000000001")
+        );
+    }
+
+    #[test]
+    fn pack_them_for_exact_input_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1_2.clone(), false),
+            hex!("0000000000000000000000000000000000000001000bb800000000000000000000000000000000000000020001f40000000000000000000000000000000000000003")
+        );
+    }
+
+    #[test]
+    fn pack_them_for_exact_output_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1_2.clone(), true),
+            hex!("00000000000000000000000000000000000000030001f40000000000000000000000000000000000000002000bb80000000000000000000000000000000000000001")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_input_for_exact_input_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_WETH_0.clone(), false),
+            hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000bb80000000000000000000000000000000000000001")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_input_for_exact_output_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_WETH_0.clone(), true),
+            hex!("0000000000000000000000000000000000000001000bb8c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_input_for_exact_input_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_WETH_0_1.clone(), false),
+            hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000bb80000000000000000000000000000000000000001000bb80000000000000000000000000000000000000002")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_input_for_exact_output_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_WETH_0_1.clone(), true),
+            hex!("0000000000000000000000000000000000000002000bb80000000000000000000000000000000000000001000bb8c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_output_for_exact_input_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_WETH.clone(), false),
+            hex!("0000000000000000000000000000000000000001000bb8c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_output_for_exact_output_single_hop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_WETH.clone(), true),
+            hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000bb80000000000000000000000000000000000000001")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_output_for_exact_input_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1_WETH.clone(), false),
+            hex!("0000000000000000000000000000000000000001000bb80000000000000000000000000000000000000002000bb8c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2")
+        );
+    }
+
+    #[test]
+    fn wrap_ether_output_for_exact_output_multihop() {
+        assert_eq!(
+            encode_route_to_path(ROUTE_0_1_WETH.clone(), true),
+            hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000bb80000000000000000000000000000000000000002000bb80000000000000000000000000000000000000001")
+        );
+    }
+}
