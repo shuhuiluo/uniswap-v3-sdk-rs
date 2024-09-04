4a3d18cd15cedc1b5b65b4f7bb725e7df37faafe
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-04 03:44:54 -0400

    Refactor `encode_sqrt_ratio_x96` to use generic `Uint` type
    Standardize the `alias` usage by transitioning from `U160` to a more flexible `Uint` type throughout the `encode_sqrt_ratio_x96` function. Also, clean up the tests to align with this new generic type and ensure compatibility and extensibility for future enhancements.

--- src/utils/encode_sqrt_ratio_x96.rs
+++ src/utils/encode_sqrt_ratio_x96.rs
@@ -1,47 +1,51 @@
 use super::FromBig;
-use alloy_primitives::U160;
+use alloy_primitives::Uint;
 use num_bigint::BigInt;
 use uniswap_sdk_core::utils::sqrt::sqrt;
 
-/// Returns the sqrt ratio as a Q64.96 corresponding to a given ratio of amount1 and amount0
+/// Returns the sqrt ratio as a Q64.96 corresponding to a given ratio of `amount1` and `amount0`.
 ///
 /// ## Arguments
 ///
 /// * `amount1`: The numerator amount i.e., the amount of token1
 /// * `amount0`: The denominator amount i.e., the amount of token0
 ///
-/// returns: U160 The sqrt ratio as a Q64.96
-pub fn encode_sqrt_ratio_x96(amount1: impl Into<BigInt>, amount0: impl Into<BigInt>) -> U160 {
+/// ## Returns
+///
+/// The sqrt ratio as a Q64.96
+#[inline]
+pub fn encode_sqrt_ratio_x96<const BITS: usize, const LIMBS: usize>(
+    amount1: impl Into<BigInt>,
+    amount0: impl Into<BigInt>,
+) -> Uint<BITS, LIMBS> {
     let numerator = amount1.into() << 192;
     let denominator = amount0.into();
-    U160::from_big_int(sqrt(&(numerator / denominator)).unwrap())
+    Uint::from_big_int(sqrt(&(numerator / denominator)).unwrap())
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::utils::{encode_sqrt_ratio_x96, Q96};
+    use alloy_primitives::U256;
 
     #[test]
     fn test_encode_sqrt_ratio_x96() {
-        assert_eq!(
-            encode_sqrt_ratio_x96(1, 1),
-            U160::from_limbs([0, 4294967296, 0])
-        );
+        assert_eq!(encode_sqrt_ratio_x96(1, 1), Q96);
         assert_eq!(
             encode_sqrt_ratio_x96(100, 1),
-            U160::from(792281625142643375935439503360u128)
+            U256::from(792281625142643375935439503360u128)
         );
         assert_eq!(
             encode_sqrt_ratio_x96(1, 100),
-            U160::from(7922816251426433759354395033u128)
+            U256::from(7922816251426433759354395033u128)
         );
         assert_eq!(
             encode_sqrt_ratio_x96(111, 333),
-            U160::from(45742400955009932534161870629u128)
+            U256::from(45742400955009932534161870629u128)
         );
         assert_eq!(
             encode_sqrt_ratio_x96(333, 111),
-            U160::from(137227202865029797602485611888u128)
+            U256::from(137227202865029797602485611888u128)
         );
     }
 }
