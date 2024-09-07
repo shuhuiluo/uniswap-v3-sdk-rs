f443bec635d5b9ff54a7549f7a2a0195448d2b38
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-07 03:24:21 -0400

    refactor: update `Error` enum implementation
    Reorganized `Error` enum by adding new attributes and reordering elements. Incorporated `extensions` feature for more detailed error handling and adjusted attribute macros to enhance enum definition.

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,19 @@
+#[cfg(doc)]
+use crate::prelude::*;
+#[cfg(feature = "extensions")]
+use alloy::contract::Error as ContractError;
 use alloy_primitives::{aliases::I24, U160};
 use uniswap_sdk_core::error::Error as CoreError;
 
 #[allow(missing_copy_implementations)]
-#[derive(Debug)]
+#[cfg_attr(
+    not(feature = "extensions"),
+    derive(Clone, Copy, Debug, Hash, PartialEq, Eq)
+)]
+#[cfg_attr(feature = "extensions", derive(Debug))]
 #[cfg_attr(feature = "std", derive(thiserror::Error))]
 pub enum Error {
+    /// Thrown when an error occurs in the core library.
     #[cfg_attr(feature = "std", error("{0}"))]
     Core(CoreError),
 
@@ -12,15 +21,25 @@ pub enum Error {
     #[cfg_attr(feature = "std", error("Invalid token"))]
     InvalidToken,
 
-    /// Thrown when the tick passed to [`get_sqrt_price_at_tick`] is not between [`MIN_TICK`] and
+    /// Thrown when the tick passed to [`get_sqrt_ratio_at_tick`] is not between [`MIN_TICK`] and
     /// [`MAX_TICK`].
     #[cfg_attr(feature = "std", error("Invalid tick: {0}"))]
     InvalidTick(I24),
 
-    /// Thrown when the price passed to [`get_tick_at_sqrt_price`] does not correspond to a price
+    /// Thrown when the price passed to [`get_tick_at_sqrt_ratio`] does not correspond to a price
     /// between [`MIN_TICK`] and [`MAX_TICK`].
     #[cfg_attr(feature = "std", error("Invalid square root price: {0}"))]
     InvalidSqrtPrice(U160),
+
+    #[cfg_attr(feature = "std", error("Invalid price or liquidity"))]
+    InvalidPriceOrLiquidity,
+
+    #[cfg_attr(feature = "std", error("Invalid price"))]
+    InvalidPrice,
+
+    #[cfg(feature = "extensions")]
+    #[cfg_attr(feature = "std", error("Invalid tick range"))]
+    InvalidRange,
 
     #[cfg_attr(feature = "std", error("Overflow in full math mulDiv"))]
     MulDivOverflow,
@@ -37,22 +56,12 @@ pub enum Error {
     #[cfg_attr(feature = "std", error("Insufficient liquidity"))]
     InsufficientLiquidity,
 
-    #[cfg_attr(feature = "std", error("Invalid price or liquidity"))]
-    InvalidPriceOrLiquidity,
-
-    #[cfg_attr(feature = "std", error("Invalid price"))]
-    InvalidPrice,
-
     #[cfg_attr(feature = "std", error("No tick data provider was given"))]
     NoTickDataError,
 
     #[cfg(feature = "extensions")]
-    #[cfg_attr(feature = "std", error("Invalid tick range"))]
-    InvalidRange,
-
-    #[cfg(feature = "extensions")]
     #[cfg_attr(feature = "std", error("{0}"))]
-    ContractError(alloy::contract::Error),
+    ContractError(ContractError),
 
     #[cfg(feature = "extensions")]
     #[cfg_attr(feature = "std", error("Error calling lens contract"))]
@@ -66,8 +75,8 @@ impl From<CoreError> for Error {
 }
 
 #[cfg(feature = "extensions")]
-impl From<alloy::contract::Error> for Error {
-    fn from(error: alloy::contract::Error) -> Self {
+impl From<ContractError> for Error {
+    fn from(error: ContractError) -> Self {
         Error::ContractError(error)
     }
 }
