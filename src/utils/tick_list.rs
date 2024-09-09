73c3d9c21e2b060a1c73d7aefcf5e2f092ae4fa1
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-09-09 01:33:35 -0400

    refactor: tick list logic and error handling
    Enhanced the logic in `next_initialized_tick_within_one_word` and improved readability by using more idiomatic Rust patterns. Replaced `Lazy` with `const` for static ticks in tests, and updated README to reflect version bump from 0.36.0 to 0.37.0.

--- src/utils/tick_list.rs
+++ src/utils/tick_list.rs
@@ -28,7 +28,30 @@ pub trait TickList {
         tick: Self::Index,
         lte: bool,
         tick_spacing: Self::Index,
-    ) -> (Self::Index, bool);
+    ) -> (Self::Index, bool) {
+        let compressed = tick.div_floor(tick_spacing);
+        if lte {
+            let word_pos = compressed >> 8;
+            let minimum = (word_pos << 8) * tick_spacing;
+
+            if self.is_below_smallest(tick) {
+                return (minimum, false);
+            }
+            let index = self.next_initialized_tick(tick, lte).index;
+            let next_initialized_tick = minimum.max(index);
+            (next_initialized_tick, next_initialized_tick == index)
+        } else {
+            let one = Self::Index::one();
+            let word_pos = (compressed + one) >> 8;
+            let maximum = (((word_pos + one) << 8) - one) * tick_spacing;
+            if self.is_at_or_above_largest(tick) {
+                return (maximum, false);
+            }
+            let index = self.next_initialized_tick(tick, lte).index;
+            let next_initialized_tick = maximum.min(index);
+            (next_initialized_tick, next_initialized_tick == index)
+        }
+    }
 }
 
 impl<I: TickIndex> TickList for [Tick<I>] {
@@ -53,24 +76,28 @@ impl<I: TickIndex> TickList for [Tick<I>] {
     }
 
     fn is_below_smallest(&self, tick: I) -> bool {
-        assert!(!self.is_empty(), "LENGTH");
-        tick < self[0].index
+        match self.first() {
+            Some(first) => tick < first.index,
+            None => panic!("LENGTH"),
+        }
     }
 
     fn is_at_or_above_largest(&self, tick: I) -> bool {
-        assert!(!self.is_empty(), "LENGTH");
-        tick >= self.last().unwrap().index
+        match self.last() {
+            Some(last) => tick >= last.index,
+            None => panic!("LENGTH"),
+        }
     }
 
     fn get_tick(&self, index: I) -> &Tick<I> {
-        let i = Self::binary_search_by_tick(self, index);
+        let i = self.binary_search_by_tick(index);
         let tick = &self[i];
         assert_eq!(tick.index, index, "NOT_CONTAINED");
         tick
     }
 
     fn binary_search_by_tick(&self, tick: I) -> usize {
-        assert!(!Self::is_below_smallest(self, tick), "BELOW_SMALLEST");
+        assert!(!self.is_below_smallest(tick), "BELOW_SMALLEST");
         let mut l = 0;
         let mut r = self.len() - 1;
 
@@ -89,52 +116,19 @@ impl<I: TickIndex> TickList for [Tick<I>] {
 
     fn next_initialized_tick(&self, tick: I, lte: bool) -> &Tick<I> {
         if lte {
-            assert!(!Self::is_below_smallest(self, tick), "BELOW_SMALLEST");
-            if Self::is_at_or_above_largest(self, tick) {
+            assert!(!self.is_below_smallest(tick), "BELOW_SMALLEST");
+            if self.is_at_or_above_largest(tick) {
                 return self.last().unwrap();
             }
-            let index = Self::binary_search_by_tick(self, tick);
+            let index = self.binary_search_by_tick(tick);
             &self[index]
         } else {
-            assert!(
-                !Self::is_at_or_above_largest(self, tick),
-                "AT_OR_ABOVE_LARGEST"
-            );
-            if Self::is_below_smallest(self, tick) {
+            assert!(!self.is_at_or_above_largest(tick), "AT_OR_ABOVE_LARGEST");
+            if self.is_below_smallest(tick) {
                 return &self[0];
             }
-            let index = Self::binary_search_by_tick(self, tick);
+            let index = self.binary_search_by_tick(tick);
             &self[index + 1]
-        }
-    }
-
-    fn next_initialized_tick_within_one_word(
-        &self,
-        tick: I,
-        lte: bool,
-        tick_spacing: I,
-    ) -> (I, bool) {
-        let compressed = tick.div_floor(tick_spacing);
-        if lte {
-            let word_pos = compressed >> 8;
-            let minimum = (word_pos << 8) * tick_spacing;
-
-            if Self::is_below_smallest(self, tick) {
-                return (minimum, false);
-            }
-            let index = Self::next_initialized_tick(self, tick, lte).index;
-            let next_initialized_tick = minimum.max(index);
-            (next_initialized_tick, next_initialized_tick == index)
-        } else {
-            let one = I::one();
-            let word_pos = (compressed + one) >> 8;
-            let maximum = (((word_pos + one) << 8) - one) * tick_spacing;
-            if Self::is_at_or_above_largest(self, tick) {
-                return (maximum, false);
-            }
-            let index = Self::next_initialized_tick(self, tick, lte).index;
-            let next_initialized_tick = maximum.min(index);
-            (next_initialized_tick, next_initialized_tick == index)
         }
     }
 }
@@ -146,12 +140,23 @@ mod tests {
         entities::Tick,
         utils::{MAX_TICK_I32 as MAX_TICK, MIN_TICK_I32 as MIN_TICK},
     };
-    use once_cell::sync::Lazy;
 
-    static LOW_TICK: Lazy<Tick> = Lazy::new(|| Tick::new(MIN_TICK + 1, 10, 10));
-    static MID_TICK: Lazy<Tick> = Lazy::new(|| Tick::new(0, 5, -5));
-    static HIGH_TICK: Lazy<Tick> = Lazy::new(|| Tick::new(MAX_TICK - 1, 5, -5));
-    static TICKS: Lazy<[Tick; 3]> = Lazy::new(|| [*LOW_TICK, *MID_TICK, *HIGH_TICK]);
+    const LOW_TICK: Tick = Tick {
+        index: MIN_TICK + 1,
+        liquidity_gross: 10,
+        liquidity_net: 10,
+    };
+    const MID_TICK: Tick = Tick {
+        index: 0,
+        liquidity_gross: 5,
+        liquidity_net: -5,
+    };
+    const HIGH_TICK: Tick = Tick {
+        index: MAX_TICK - 1,
+        liquidity_gross: 5,
+        liquidity_net: -5,
+    };
+    const TICKS: [Tick; 3] = [LOW_TICK, MID_TICK, HIGH_TICK];
 
     #[test]
     fn test_impl_for_vec() {
@@ -164,19 +169,19 @@ mod tests {
     #[test]
     #[should_panic(expected = "ZERO_NET")]
     fn test_validate_list_zero_net() {
-        [*LOW_TICK].validate_list(1);
+        [LOW_TICK].validate_list(1);
     }
 
     #[test]
     #[should_panic(expected = "SORTED")]
     fn test_validate_list_unsorted() {
-        [*HIGH_TICK, *LOW_TICK, *MID_TICK].validate_list(1);
+        [HIGH_TICK, LOW_TICK, MID_TICK].validate_list(1);
     }
 
     #[test]
     #[should_panic(expected = "TICK_SPACING")]
     fn test_validate_list_tick_spacing() {
-        [*HIGH_TICK, *LOW_TICK, *MID_TICK].validate_list(1337);
+        [HIGH_TICK, LOW_TICK, MID_TICK].validate_list(1337);
     }
 
     #[test]
@@ -199,32 +204,32 @@ mod tests {
 
     #[test]
     fn test_next_initialized_tick_low_lte_true_2() {
-        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 1, true), &*LOW_TICK);
-        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 2, true), &*LOW_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 1, true), &LOW_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 2, true), &LOW_TICK);
     }
 
     #[test]
     fn test_next_initialized_tick_low_lte_false() {
-        assert_eq!(TICKS.next_initialized_tick(MIN_TICK, false), &*LOW_TICK);
-        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 1, false), &*MID_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MIN_TICK, false), &LOW_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MIN_TICK + 1, false), &MID_TICK);
     }
 
     #[test]
     fn test_next_initialized_tick_mid_lte_true() {
-        assert_eq!(TICKS.next_initialized_tick(0, true), &*MID_TICK);
-        assert_eq!(TICKS.next_initialized_tick(1, true), &*MID_TICK);
+        assert_eq!(TICKS.next_initialized_tick(0, true), &MID_TICK);
+        assert_eq!(TICKS.next_initialized_tick(1, true), &MID_TICK);
     }
 
     #[test]
     fn test_next_initialized_tick_mid_lte_false() {
-        assert_eq!(TICKS.next_initialized_tick(-1, false), &*MID_TICK);
-        assert_eq!(TICKS.next_initialized_tick(1, false), &*HIGH_TICK);
+        assert_eq!(TICKS.next_initialized_tick(-1, false), &MID_TICK);
+        assert_eq!(TICKS.next_initialized_tick(1, false), &HIGH_TICK);
     }
 
     #[test]
     fn test_next_initialized_tick_high_lte_true() {
-        assert_eq!(TICKS.next_initialized_tick(MAX_TICK - 1, true), &*HIGH_TICK);
-        assert_eq!(TICKS.next_initialized_tick(MAX_TICK, true), &*HIGH_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MAX_TICK - 1, true), &HIGH_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MAX_TICK, true), &HIGH_TICK);
     }
 
     #[test]
@@ -235,14 +240,8 @@ mod tests {
 
     #[test]
     fn test_next_initialized_tick_high_lte_false_2() {
-        assert_eq!(
-            TICKS.next_initialized_tick(MAX_TICK - 2, false),
-            &*HIGH_TICK
-        );
-        assert_eq!(
-            TICKS.next_initialized_tick(MAX_TICK - 3, false),
-            &*HIGH_TICK
-        );
+        assert_eq!(TICKS.next_initialized_tick(MAX_TICK - 2, false), &HIGH_TICK);
+        assert_eq!(TICKS.next_initialized_tick(MAX_TICK - 3, false), &HIGH_TICK);
     }
 
     #[test]
