39304d993beab5b86c5095405e5f95886e1c66a1
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-10-12 05:51:47 -0400

    feat: add multicall decoding
    Introduce `decode_multicall` function to decode encoded multicall data. Reorganized tests into `encode` and `decode` modules for clarity and thorough testing.

--- src/multicall.rs
+++ src/multicall.rs
@@ -12,26 +12,58 @@ pub fn encode_multicall(data: Vec<Bytes>) -> Bytes {
     }
 }
 
+#[inline]
+#[must_use]
+pub fn decode_multicall(encoded: &Bytes) -> Vec<Bytes> {
+    IMulticall::multicallCall::abi_decode(encoded.as_ref(), true)
+        .unwrap()
+        .data
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use alloy_primitives::hex;
 
-    #[test]
-    fn test_encode_multicall_string_array_len_1() {
-        let calldata = encode_multicall(vec![vec![0x01].into()]);
-        assert_eq!(calldata, vec![0x01]);
+    mod encode {
+        use super::*;
+
+        #[test]
+        fn test_string_array_len_1() {
+            let calldata = encode_multicall(vec![vec![0x01].into()]);
+            assert_eq!(calldata, vec![0x01]);
+        }
+
+        #[test]
+        fn test_string_array_len_2() {
+            let calldata = encode_multicall(vec![
+                hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").into(),
+                hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb").into(),
+            ]);
+            assert_eq!(
+                calldata.to_vec(),
+                hex!("ac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000020aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0000000000000000000000000000000000000000000000000000000000000020bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            );
+        }
     }
 
-    #[test]
-    fn test_encode_multicall_string_array_len_2() {
-        let calldata = encode_multicall(vec![
-            hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").into(),
-            hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb").into(),
-        ]);
-        assert_eq!(
-            calldata.to_vec(),
-            hex!("ac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000020aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0000000000000000000000000000000000000000000000000000000000000020bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
-        );
+    mod decode {
+        use super::*;
+
+        #[test]
+        fn test_string_array_len_2() {
+            let calldatas = vec![
+                hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").into(),
+                hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb").into(),
+            ];
+            let multicall = encode_multicall(calldatas.clone());
+            assert_eq!(
+                multicall.to_vec(),
+                hex!("ac9650d800000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000020aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa0000000000000000000000000000000000000000000000000000000000000020bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            );
+
+            let decoded_calldata = decode_multicall(&multicall);
+            assert_eq!(decoded_calldata, calldatas);
+        }
     }
 }
