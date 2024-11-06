ec6a972d3678aa7c68bef21bb035fa795e2fdf6d
Shuhui Luo <107524008+shuhuiluo@users.noreply.github.com>
2024-11-06 03:05:45 -0500

    refactor!: update error handling and dependencies (#103)
    * refactor!: update error handling and dependencies
    
    Removed unnecessary `?` operator in `trade.rs` when calling `sorted_insert`. Replaced `derive_more::From` with `thiserror::Error` in `error.rs` to improve error handling. Upgraded dependencies and updated version numbers in `Cargo.toml` and `README.md` to reflect the new changes.
    
    * Fix error messages in src/error.rs
    
    Update error messages for `MulDivOverflow` and `AddDeltaOverflow` to provide more accurate descriptions. This fix ensures better clarity and correctness in the error reporting.

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::missing_inline_in_public_items)]
+
 #[cfg(doc)]
 use crate::prelude::*;
 
@@ -7,78 +9,75 @@ use alloy::contract::Error as ContractError;
 use uniswap_lens::error::Error as LensError;
 
 use alloy_primitives::{aliases::I24, U160};
-use derive_more::From;
 use uniswap_sdk_core::error::Error as CoreError;
 
-#[derive(Debug, From)]
+#[derive(Debug, thiserror::Error)]
 #[cfg_attr(not(feature = "extensions"), derive(Clone, Copy, Hash, PartialEq, Eq))]
-#[cfg_attr(feature = "std", derive(thiserror::Error))]
 pub enum Error {
     /// Thrown when an error occurs in the core library.
-    #[cfg_attr(feature = "std", error("{0}"))]
-    Core(#[cfg_attr(not(feature = "std"), from)] CoreError),
+    #[error("{0}")]
+    Core(#[from] CoreError),
 
     /// Thrown when the token passed to [`Pool::price_of`] is not one of the pool's tokens.
-    #[cfg_attr(feature = "std", error("Invalid token"))]
+    #[error("Invalid token")]
     InvalidToken,
 
     /// Thrown when the tick passed to [`get_sqrt_ratio_at_tick`] is not between [`MIN_TICK`] and
     /// [`MAX_TICK`].
-    #[cfg_attr(feature = "std", error("Invalid tick: {0}"))]
+    #[error("Invalid tick: {0}")]
     InvalidTick(I24),
 
     /// Thrown when the price passed to [`get_tick_at_sqrt_ratio`] does not correspond to a price
     /// between [`MIN_TICK`] and [`MAX_TICK`].
-    #[cfg_attr(feature = "std", error("Invalid square root price: {0}"))]
+    #[error("Invalid square root price: {0}")]
     InvalidSqrtPrice(U160),
 
-    #[cfg_attr(feature = "std", error("Invalid price or liquidity"))]
+    #[error("Invalid price or liquidity")]
     InvalidPriceOrLiquidity,
 
-    #[cfg_attr(feature = "std", error("Invalid price"))]
+    #[error("Invalid price")]
     InvalidPrice,
 
     #[cfg(feature = "extensions")]
-    #[cfg_attr(feature = "std", error("Invalid tick range"))]
+    #[error("Invalid tick range")]
     InvalidRange,
 
-    #[cfg_attr(feature = "std", error("Overflow in full math mulDiv"))]
+    #[error("Overflow in full math mulDiv")]
     MulDivOverflow,
 
-    #[cfg_attr(feature = "std", error("Overflow when adding liquidity delta"))]
+    #[error("Overflow when adding liquidity delta")]
     AddDeltaOverflow,
 
-    #[cfg_attr(feature = "std", error("Overflow when casting to U160"))]
+    #[error("Overflow when casting to U160")]
     SafeCastToU160Overflow,
 
-    #[cfg_attr(feature = "std", error("Overflow in price calculation"))]
+    #[error("Overflow in price calculation")]
     PriceOverflow,
 
-    #[cfg_attr(feature = "std", error("Insufficient liquidity"))]
+    #[error("Insufficient liquidity")]
     InsufficientLiquidity,
 
-    #[cfg_attr(feature = "std", error("No tick data provider was given"))]
+    #[error("No tick data provider was given")]
     NoTickDataError,
 
     #[cfg(feature = "extensions")]
-    #[cfg_attr(feature = "std", error("{0}"))]
-    ContractError(#[cfg_attr(not(feature = "std"), from)] ContractError),
+    #[error("{0}")]
+    ContractError(#[from] ContractError),
 
     #[cfg(feature = "extensions")]
-    #[cfg_attr(feature = "std", error("{0}"))]
-    LensError(#[cfg_attr(not(feature = "std"), from)] LensError),
+    #[error("{0}")]
+    LensError(#[from] LensError),
 
-    #[cfg_attr(feature = "std", error("{0}"))]
-    TickListError(#[cfg_attr(not(feature = "std"), from)] TickListError),
+    #[error("{0}")]
+    TickListError(#[from] TickListError),
 }
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
-#[cfg_attr(feature = "std", derive(thiserror::Error))]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, thiserror::Error)]
 pub enum TickListError {
-    #[cfg_attr(feature = "std", error("Below smallest tick"))]
+    #[error("Below smallest tick")]
     BelowSmallest,
-    #[cfg_attr(feature = "std", error("At or above largest tick"))]
+    #[error("At or above largest tick")]
     AtOrAboveLargest,
-    #[cfg_attr(feature = "std", error("Not contained in tick list"))]
+    #[error("Not contained in tick list")]
     NotContained,
 }
